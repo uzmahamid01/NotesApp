9e67d777ea198f761ca2d9d9f2805cc704cff9bf
Uzma Hamid <95444550+uzmahamid01@users.noreply.github.com>
2024-06-07 16:38:25 -0500

    sync, modify, title, delete added

--- App.js
+++ App.js
@@ -20,10 +20,11 @@ export default function App() {
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
-    const [state, setState] = React.useState(console.log("state initialization"))
+    // const [state, setState] = React.useState(console.log("state initialization"))
 
     React.useEffect(() => {
         localStorage.setItem("notes", JSON.stringify(notes))
+        console.log(notes[0].body.split("\n"))
     }, [notes])
     
     function createNewNote() {
@@ -37,11 +38,53 @@ export default function App() {
     
     function updateNote(text) {
         setNotes(oldNotes => oldNotes.map(oldNote => {
-            return oldNote.id === currentNoteId
-                ? { ...oldNote, body: text }
-                : oldNote
+            for (let i = 0; i < oldNote.length; i++){
+                const oldNotes = oldNote[i]
+                if(oldNote.id === currentNoteId ){
+                    newArray.unshift({ ...oldNote, body: text})
+                } 
+                else{
+                    newArray.push(oldNote)
+                }
+            }
+            return newArray
+            //create a new emmpty array
+            //loop over the origibal array
+                //if the id matches  
+                    //puth the updated note at the 
+                    //beginning of the new array
+                //else
+                    //push the old note to the end
+                    //of the new arra
+
         }))
     }
+
+    //this does not rearrange the notes
+    // setNotes(oldNotes => oldNotes.map(oldNote => {
+    //     return oldNote.id === currentNoteId
+    //         ? { ...oldNote, body: text }
+    //         : oldNote
+    // }))
+
+     /**
+     * Challenge: complete and implement the deleteNote function
+     * 
+     * Hints: 
+     * 1. What array method can be used to return a new
+     *    array that has filtered out an item based 
+     *    on a condition?
+     * 2. Notice the parameters being based to the function
+     *    and think about how both of those parameters
+     *    can be passed in during the onClick event handler
+     */
+    
+    function deleteNote(event, noteId) {
+        event.stopPropagation()
+        // Your code here
+        setNotes(oldNote => oldNotes.filter(note =>note.id !== noteId))
+    }
+
     
     function findCurrentNote() {
         return notes.find(note => {
